cabea07c7ce285b5e9b3eb1f95e059670fbb9a0d
Lucas Waltz <lucaswaltz7@gmail.com>
2023-07-11 08:23:02 -0300

    header adicionado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import LogoImg from './assets/logo.svg'
+import ProfileImg from './assets/profile.jpg'
 
 function App() {
   return (
     <div className="container">
 
-    </div>
+      <header>
+        <img src={LogoImg} />
+
+        <div className='profile'>
+          <img src={ProfileImg} />
+          <span>Bem vindo, Lucas.</span>
+        </div>
+
+      </header>
+
+    </div >
   );
 }
 
